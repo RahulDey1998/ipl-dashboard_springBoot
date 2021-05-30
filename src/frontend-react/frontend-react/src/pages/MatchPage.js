6c4196676c886bffdbd9b2cb058fb164f2754c47
rahuldey1998 <deyrahul123456@gmail.com>
2021-05-30 19:41:22 +0530

    Added Match Page UI

--- src/frontend-react/frontend-react/src/pages/MatchPage.js
+++ src/frontend-react/frontend-react/src/pages/MatchPage.js
@@ -5,9 +5,31 @@ import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const MatchPage = () => {
 
+    const [matches, setMatches] = useState([]);
+    const  { teamName, year } = useParams();
+
+    useEffect(
+        () => {
+            const fetchMatches = async () => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}/matches/?year=${year}`);
+                const data = await response.json();           
+                setMatches(data);                 
+            };
+            fetchMatches(); 
+        }, []
+    );
+    if(matches.length == 0) {
+        return (
+            <h3>Match not found.</h3>
+        );
+    }
+
     return (
         <div className="MatchPage">
           <h1>Match Page</h1>
+          {
+              matches.map(match => <MatchDetatilsCard teamName={teamName} match={match} />)
+          }
         </div>
     );
 }
--- src/main/java/com/rahul/ipldashboard/controller/TeamController.java
+++ src/main/java/com/rahul/ipldashboard/controller/TeamController.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jdk.vm.ci.meta.Local;
-
 @RestController
 @RequestMapping("/team")
 @CrossOrigin
